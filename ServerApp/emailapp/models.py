11849548f856f7adbe8e46fd66fcbe7125e1963e
Tanisha Prakash <106842989+Moriarty1056@users.noreply.github.com>
2023-03-21 02:19:27 +0530

    multiple receivers

--- ServerApp/emailapp/models.py
+++ ServerApp/emailapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Email(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_emails')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_emails')
+    receiver = models.ManyToManyField(User,  related_name='received_emails')
     subject = models.CharField(max_length=200)
     body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
