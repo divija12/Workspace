fdc7659033df10554639ca183934d62a4634fea5
divija12 <kejriwaldivija12@gmail.com>
2023-03-21 14:45:08 +0530

    single user

--- ServerApp/emailapp/forms.py
+++ ServerApp/emailapp/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import Email
 
 class EmailForm(forms.ModelForm):
-    receiver = forms.ModelMultipleChoiceField(queryset=User.objects.all(),widget=forms.CheckboxSelectMultiple())
+    receiver = forms.ModelChoiceField(queryset=User.objects.all())
     subject = forms.CharField(max_length=200)
     body = forms.CharField(widget=forms.Textarea)
 
