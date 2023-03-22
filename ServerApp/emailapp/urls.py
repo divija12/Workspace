b0f21a8e13341301a1de3448b5647f1a68205c9f
divija12 <kejriwaldivija12@gmail.com>
2023-03-23 02:14:47 +0530

    modified

--- ServerApp/emailapp/urls.py
+++ ServerApp/emailapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('email/<int:email_id>/', email_detail, name='email_detail'),
     path('<int:email_id>/reply/', reply_to_email, name='reply_email'),
     path('delete/<int:email_id>/', delete_email, name='delete_email'),
+    path('read_unread/<int:email_id>/', mark_as_read_unread, name='mark_as_read_unread'),
 ]
 
